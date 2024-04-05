c1af7d277ff38014654591aceb48ace58fc2b8ac
Juan David Cutiva <69434726+Juan-Cutiva@users.noreply.github.com>
2024-04-05 14:16:26 -0500

    Arreglo de el Load al inicio

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ let vidasEnemigo = 3
 let mascotaEnemigo
 let mascotaJugador
 
-function iniciarJuago(){    
+function iniciarJuego(){    
     let sectionReiniciar = document.getElementById("reiniciar")
     sectionReiniciar.style.display="none"
     
@@ -161,4 +161,4 @@ function aleatorio(min, max){
     return Math.floor(Math.random() * (max - min + 1) + min)
 }
 
-window.addEventListener("load",iniciarJuago)
+window.addEventListener("load",iniciarJuego)
