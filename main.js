c8b2c165e9abe9596cac7cec9bbc83c502292d15
Juan David Cutiva <69434726+Juan-Cutiva@users.noreply.github.com>
2024-04-19 12:47:12 -0500

    Ya todo funciona Siuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuu

--- main.js
+++ main.js
@@ -265,6 +265,23 @@ function enviarAtaques(){
             ataques: ataqueJugador
         })
     })
+
+    intervalo = setInterval(obtenerAtaques, 50)
+}
+
+function obtenerAtaques(){
+    fetch(`http://localhost:8080/mokepon/${enemigoId}/ataques`)
+        .then(function(res){
+            if (res.ok){
+                res.json()
+                    .then(function({ataques}){
+                        if (ataques.length === 5){
+                            ataqueEnemigo = ataques
+                            combate()
+                        }
+                    })
+            }
+        })
 }
 
 function seleccionarMascotaEnemigo(enemigo){
@@ -297,6 +314,7 @@ function iAmbosOponentes(jugador, enemigo){
 }
 
 function combate(){
+    clearInterval(intervalo)
     
     for (let i = 0; i < ataqueJugador.length; i++) {
         if(ataqueJugador[i] === ataqueEnemigo[i]){
