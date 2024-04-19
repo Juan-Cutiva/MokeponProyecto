86f4c524dff284dd218fed9d5491691ebacde70a
Juan David Cutiva <69434726+Juan-Cutiva@users.noreply.github.com>
2024-04-18 19:06:07 -0500

    Aca todo va bien, solo falta que funcionen las coliciones

--- main.js
+++ main.js
@@ -371,8 +371,13 @@ function pintarCanvas(){
 
     mokeponesEnemigos.forEach(function(mokepon){
         mokepon.pintarMokepon()
-        revisarColision(mokepon)
     })
+    
+    if(mascotaJugadorObjeto.velocidadX !== 0 || mascotaJugadorObjeto.velocidadY !== 0){
+        revisarColision(hipodogeEnemigo)
+        revisarColision(capipepoEnemigo)
+        revisarColision(ratigueyaEnemigo)
+    }
 }
 
 function enviarPosicion(x, y){
